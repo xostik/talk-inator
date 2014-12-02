5261c6e1865efdf0a7d426ae462d6ec1cb4f9152
xostik <xostiki@ya.ru>
2014-12-02 23:58:33 +0500

    файлецы

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+﻿module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.initConfig({
+        less : {
+            default: {
+                src: ['styles/less/talk.less'],
+                dest: 'styles/css/main.css'
+            }
+        }
+    });
+};
